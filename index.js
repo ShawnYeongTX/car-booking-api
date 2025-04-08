af857d9aec12f547a18aa97227a44cc7fe99ef5d
Shawn <ShawnYeong1016@gmail.com>
2025-04-08 15:42:39 +0800

    reconfig vercel

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
-const { Pool } = require("pg");
 require("dotenv").config();
 
 let app = express();
