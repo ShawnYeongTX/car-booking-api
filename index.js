a53df825ebbeb81741b671677332928b66047976
Shawn <ShawnYeong1016@gmail.com>
2025-04-23 10:30:13 +0800

    add total_price update for api

--- index.js
+++ index.js
@@ -152,11 +152,18 @@ app.put("/booking/:id", async (req, res) => {
     // Update the booking
     const query = `
     UPDATE bookings
-    SET start_date = $1, end_date = $2, name = $3, contact = $4
-    WHERE id = $5
-    RETURNING id, start_date, end_date, name, contact
+    SET start_date = $1, end_date = $2, total_price = $3, name = $4, contact = $5
+    WHERE id = $6
+    RETURNING id, start_date, end_date, total_price, name, contact
   `;
-    const params = [start_date, end_date, name, contact, bookingId];
+    const params = [
+      start_date,
+      end_date,
+      total_price,
+      name,
+      contact,
+      bookingId,
+    ];
 
     const result = await client.query(query, params);
 
